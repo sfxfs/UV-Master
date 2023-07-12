27122a4fe76b8d2878c1ea8713be2b7c6de3d0dc
sfxfs <2501683578@qq.com>
2023-07-12 11:11:44 +0800

    完善 README

--- user/server/handler_function.c
+++ user/server/handler_function.c
@@ -2,7 +2,6 @@
 // Created by fxf on 23-7-9.
 //
 
-#include "../../protocols/cJSON.h"
 #include "handler_function.h"
 
 cJSON *get_rov_info(sensor_t *sensor_data)
--- user/server/handler_function.h
+++ user/server/handler_function.h
@@ -5,6 +5,7 @@
 #ifndef ROV_MASTER_HANDLER_FUNCTION_H
 #define ROV_MASTER_HANDLER_FUNCTION_H
 
+#include "../../protocols/cJSON.h"
 #include "../data_define.h"
 
 cJSON *get_rov_info(sensor_t *sensor_data);
